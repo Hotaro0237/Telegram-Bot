8077961de954d83230eaf09d5b7a94d6a3008b7d
Hotaro0237 <ujjwalrawata@gmail.com>
2023-03-29 13:10:36 +0530

    1.1

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const bot = new Telegraf('5383319279:AAHozd2gIDj3ql2ZH2GVxVHCq2No_WWoIpo');
 const YOUR_API_KEY = '4efa5d4ad649afc087650a8168c8a9c1'
 
 const help = `Hi this is Hotaro 
-/weather : to see the weather at Haldwani 
+/weather <City_name> : to see the weather at any location 
 /help : to see this`
 
 bot.start((ctx) => {
   ctx.reply(help)
-  sendWeatherReport(ctx.chat.id)
 });
 
 bot.command('help', (ctx) => {
@@ -18,11 +17,12 @@ bot.command('help', (ctx) => {
 })
 
 const chatId = '1628311206'
-const location = 'Haldwani'
+// const location = 'Haldwani'
 
 bot.command('weather', async (ctx) => {
   // const location = ctx.message.text.slice(9)
-  
+  const location = ctx.message.text.split(' ')[1];
+  if (!location) return ctx.reply('Please specify a city');
   async function sendWeatherReport(chatId) {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${YOUR_API_KEY}&units=metric`
     
@@ -33,7 +33,7 @@ bot.command('weather', async (ctx) => {
       
       const message =`Weather Report: 
       Location : ${location}
-      Temp. : ${temp}°C
+      Temp. : ${temp}°C 
       Feels Like : ${feels_like}°C
       Humidity : ${humidity}%
       Weather : ${description}`
@@ -50,4 +50,4 @@ sendWeatherReport(ctx.chat.id)
 cron.schedule('0 6 * * *', () => {
   sendWeatherReport(chatId)
 })
-bot.launch();
+bot.launch(); 
