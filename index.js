f29cc5d571f3a9095934e25fcbcf91dc679ebf9c
Hotaro0237 <ujjwalrawata@gmail.com>
2023-08-28 21:24:27 +0530

    updated

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const dotenv = require('dotenv')
 const { Telegraf } = require('telegraf');
 const axios = require('axios')
-const cron = require('node-cron')
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.Bot_Token)
 const YOUR_API_KEY = process.env.YOUR_API_KEY
 
 const help = `Hi this is Hotaro 
